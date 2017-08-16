819d95456dc78cc278a5c13416e9ca79d32bc591
Stephen Rosenthal <stephen.rosenthal@seeq.com>
2017-08-16 12:07:45 -0400

    Install JSCS and run node_modules/jscs/bin/jscs --fix src/*.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var path = require("path"),  
-    express = require("express");
+var path = require('path'),
+    express = require('express');
 
-var DIST_DIR = path.join(__dirname, "dist"),  
+var DIST_DIR = path.join(__dirname, 'dist'),
     PORT = process.env.PORT || 3000,
     app = express();
 
@@ -9,8 +9,8 @@ var DIST_DIR = path.join(__dirname, "dist"),
 app.use(express.static(DIST_DIR));
 
 //Send index.html when the user access the web
-app.get("*", function (req, res) {  
-  res.sendFile(path.join(DIST_DIR, "index.html"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(DIST_DIR, 'index.html'));
 });
 
 app.listen(PORT);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Matter from 'matter-js'
+import Matter from 'matter-js';
 
 const KEY_W = 87;
 const KEY_A = 65;
@@ -17,31 +17,30 @@ const RED = '#F45252';
 const BLUE = '#409BE7';
 const YELLOW = '#FFEF99';
 
-
-var c = document.getElementById("cv");
-var ctx = c.getContext("2d");
+var c = document.getElementById('cv');
+var ctx = c.getContext('2d');
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 
 window.onresize = function(event) {
     c.width = window.innerWidth;
     c.height = window.innerHeight;
-};
+  };
 
 var mouseIsDown = false;
 var mp;
 var keys = [];
 
-    // module aliases
-var Engine = Matter.Engine,
-      World = Matter.World,
-      Composites = Matter.Composites,
-      Composite = Matter.Composite,
-      Body = Matter.Body,
-      Bodies = Matter.Bodies,
-      Events = Matter.Events,
-      MouseConstraint = Matter.MouseConstraint,
-      Mouse = Matter.Mouse;
+// module aliases
+var Engine = Matter.Engine;
+var World = Matter.World;
+var Composites = Matter.Composites;
+var Composite = Matter.Composite;
+var Body = Matter.Body;
+var Bodies = Matter.Bodies;
+var Events = Matter.Events;
+var MouseConstraint = Matter.MouseConstraint;
+var Mouse = Matter.Mouse;
 
 // create an engine
 var engine = Engine.create();
@@ -50,66 +49,65 @@ var engine = Engine.create();
 engine.world.gravity.y = 0.0;
 
 // hook in mouse control
-var mouseConstraint = MouseConstraint.create(engine, { element: c });
+var mouseConstraint = MouseConstraint.create(engine, {element: c});
 
 // Add lots of balls
-var miniBalls = Composites.stack(window.innerWidth/2 -75, 150, 15, 15, 2, 2, function(x, y) {
-    return Bodies.circle(x, y, 3,{friction:0});
-});
+var miniBalls = Composites.stack(window.innerWidth / 2 - 75, 150, 15, 15, 2, 2, function(x, y) {
+    return Bodies.circle(x, y, 3, {friction: 0});
+  });
 
-var player1Body = Bodies.circle(window.innerWidth/2,225,10,{density:0.002, friction:0.5});
+var player1Body = Bodies.circle(window.innerWidth / 2, 225, 10, {density: 0.002, friction: 0.5});
 player1Body.col = RED;
 
 var player1 = Body.create({
             parts: [player1Body],
-            friction:0
-});
+            friction: 0
+          });
 
-var player2Body = Bodies.circle(window.innerWidth/2,225,10,{density:0.002, friction:0.5});
+var player2Body = Bodies.circle(window.innerWidth / 2, 225, 10, {density: 0.002, friction: 0.5});
 player2Body.col = BLUE;
 
 var player2 = Body.create({
             parts: [player2Body],
-            friction:0
-});
+            friction: 0
+          });
 
-
-var wall = Bodies.rectangle(window.innerWidth/2,  window.innerHeight /2, 500, 20, {
+var wall = Bodies.rectangle(window.innerWidth / 2,  window.innerHeight / 2, 500, 20, {
     isStatic: true,
-    angle:0.2
-});
+    angle: 0.2
+  });
 
-var wall2 = Bodies.rectangle(window.innerWidth/2,  window.innerHeight /2, 500, 20, {
+var wall2 = Bodies.rectangle(window.innerWidth / 2,  window.innerHeight / 2, 500, 20, {
     isStatic: true,
-    angle:-0.2
-});
+    angle: -0.2
+  });
 World.add(engine.world, [miniBalls, wall, wall2, player1, player2]);
 
 var offset = 1;
 var wallSize = 20;
 World.add(engine.world, [
     //top
-    Bodies.rectangle(window.innerWidth/2, -offset, window.innerWidth + 2 * offset, wallSize, {
+    Bodies.rectangle(window.innerWidth / 2, -offset, window.innerWidth + 2 * offset, wallSize, {
         isStatic: true
-    }),
+      }),
     //bottom
-    Bodies.rectangle(window.innerWidth/2, window.innerHeight + offset, window.innerWidth + 2 * offset, wallSize, {
+    Bodies.rectangle(window.innerWidth / 2, window.innerHeight + offset, window.innerWidth + 2 * offset, wallSize, {
         isStatic: true
-    }),
+      }),
     //right
-    Bodies.rectangle(window.innerWidth+ offset, window.innerHeight /2, wallSize, window.innerHeight + 2 * offset, {
+    Bodies.rectangle(window.innerWidth + offset, window.innerHeight / 2, wallSize, window.innerHeight + 2 * offset, {
         isStatic: true
-    }),
+      }),
     // left
-    Bodies.rectangle(-offset, window.innerHeight /2, wallSize, window.innerHeight + 2 * offset, {
+    Bodies.rectangle(-offset, window.innerHeight / 2, wallSize, window.innerHeight + 2 * offset, {
         isStatic: true
-    })
+      })
 ]);
 
 // run the engine
 Engine.run(engine);
 
-MouseConstraint.create(engine, { element: c });
+MouseConstraint.create(engine, {element: c});
 
 //render
 (
@@ -121,45 +119,44 @@ MouseConstraint.create(engine, { element: c });
     // react to key commands and apply force as needed
 
     // Player 1 uses WASD to control
-    if(keys[KEY_W]){ // Move up
-        let force = (-0.0004 * player1.mass) ;
-        Body.applyForce(player1,player1.position,{x:0,y:force});
+    if (keys[KEY_W]) { // Move up
+      let force = (-0.0004 * player1.mass) ;
+      Body.applyForce(player1, player1.position, {x: 0,y: force});
     }
-    if(keys[KEY_S]){ // Move down
-        let force = (0.0004 * player1.mass) ;
-        Body.applyForce(player1,player1.position,{x:0,y:force});
+    if (keys[KEY_S]) { // Move down
+      let force = (0.0004 * player1.mass) ;
+      Body.applyForce(player1, player1.position, {x: 0,y: force});
     }
-    if(keys[KEY_A]){ // Move left
-        let force = (-0.0004 * player1.mass) ;
-        Body.applyForce(player1,player1.position,{x:force,y:0});
+    if (keys[KEY_A]) { // Move left
+      let force = (-0.0004 * player1.mass) ;
+      Body.applyForce(player1, player1.position, {x: force,y: 0});
     }
-    if(keys[KEY_D]){ // Move right
-        let force = (0.0004 * player1.mass) ;
-        Body.applyForce(player1,player1.position,{x:force,y:0});
+    if (keys[KEY_D]) { // Move right
+      let force = (0.0004 * player1.mass) ;
+      Body.applyForce(player1, player1.position, {x: force,y: 0});
     }
 
     // Player 2 uses WASD to control
-    if(keys[KEY_UP]){ // Move up
-        let force = (-0.0004 * player2.mass) ;
-        Body.applyForce(player2,player2.position,{x:0,y:force});
+    if (keys[KEY_UP]) { // Move up
+      let force = (-0.0004 * player2.mass) ;
+      Body.applyForce(player2, player2.position, {x: 0,y: force});
     }
-    if(keys[KEY_DOWN]){ // Move down
-        let force = (0.0004 * player2.mass) ;
-        Body.applyForce(player2,player2.position,{x:0,y:force});
+    if (keys[KEY_DOWN]) { // Move down
+      let force = (0.0004 * player2.mass) ;
+      Body.applyForce(player2, player2.position, {x: 0,y: force});
     }
-    if(keys[KEY_LEFT]){ // Move left
-        let force = (-0.0004 * player2.mass) ;
-        Body.applyForce(player2,player2.position,{x:force,y:0});
+    if (keys[KEY_LEFT]) { // Move left
+      let force = (-0.0004 * player2.mass) ;
+      Body.applyForce(player2, player2.position, {x: force,y: 0});
     }
-    if(keys[KEY_RIGHT]){ // Move right
-        let force = (0.0004 * player2.mass) ;
-        Body.applyForce(player2,player2.position,{x:force,y:0});
+    if (keys[KEY_RIGHT]) { // Move right
+      let force = (0.0004 * player2.mass) ;
+      Body.applyForce(player2, player2.position, {x: force,y: 0});
     }
-
 
     // react to mouse command and add object on click (add check to only place block wher no other blocks exist)
-    if(mouseIsDown){
-        World.add(engine.world,Bodies.rectangle(mp.x, mp.y, 20, 20, {isStatic:true}));
+    if (mouseIsDown) {
+      World.add(engine.world, Bodies.rectangle(mp.x, mp.y, 20, 20, {isStatic: true}));
     }
 
     // get all bodies
@@ -173,53 +170,52 @@ MouseConstraint.create(engine, { element: c });
     //start drawing a objects
     ctx.beginPath();
     for (var i = 0; i < bodies.length; i += 1) {
-        var vertices = bodies[i].vertices;
-        ctx.moveTo(vertices[0].x, vertices[0].y);
-        for (var j = 1; j < vertices.length; j += 1) {
-            ctx.lineTo(vertices[j].x, vertices[j].y);
-        }
-        ctx.lineTo(vertices[0].x, vertices[0].y);
+      var vertices = bodies[i].vertices;
+      ctx.moveTo(vertices[0].x, vertices[0].y);
+      for (var j = 1; j < vertices.length; j += 1) {
+        ctx.lineTo(vertices[j].x, vertices[j].y);
+      }
+      ctx.lineTo(vertices[0].x, vertices[0].y);
     }
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#DDDDDD';
     ctx.stroke();
-    ctx.fillStyle='#FAFAFF';
+    ctx.fillStyle = '#FAFAFF';
     ctx.fill();
 
     // fill player separately
     fillObject(player1Body);
     fillObject(player2Body);
 
-})();
+  })();
 
-function fillObject(object){
-    ctx.beginPath();
-    var vertices = object.vertices;
-    ctx.moveTo(vertices[0].x, vertices[0].y);
-    for (var j = 1; j < vertices.length; j += 1) {
-        ctx.lineTo(vertices[j].x, vertices[j].y);
-    }
-    ctx.lineTo(vertices[0].x, vertices[0].y);
-    ctx.fillStyle =object.col;
-    ctx.fill();
+function fillObject(object) {
+  ctx.beginPath();
+  var vertices = object.vertices;
+  ctx.moveTo(vertices[0].x, vertices[0].y);
+  for (var j = 1; j < vertices.length; j += 1) {
+    ctx.lineTo(vertices[j].x, vertices[j].y);
+  }
+  ctx.lineTo(vertices[0].x, vertices[0].y);
+  ctx.fillStyle = object.col;
+  ctx.fill();
 }
-
 
 Events.on(mouseConstraint, 'mousedown', function(event) {
     var mousePosition = event.mouse.position;
     mp = mousePosition;
     mouseIsDown = true;
-});
+  });
 
 Events.on(mouseConstraint, 'mouseup', function(event) {
     var mousePosition = event.mouse.position;
     mp = mousePosition;
     mouseIsDown = false;
-});
+  });
 
-document.body.addEventListener("keyup", function(e) {
+document.body.addEventListener('keyup', function(e) {
   keys[e.keyCode] = false;
 });
-document.body.addEventListener("keydown", function(e) {
+document.body.addEventListener('keydown', function(e) {
   keys[e.keyCode] = true;
 });
