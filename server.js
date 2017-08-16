17c85c69cb76f8e4ab6d9c4e29edd8d666ce173f
Stephen Rosenthal <stephen.rosenthal@seeq.com>
2017-08-16 10:51:57 -0400

    Set port to process.env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require("path"),
     express = require("express");
 
 var DIST_DIR = path.join(__dirname, "dist"),  
-    PORT = 3000,
+    PORT = process.env.PORT || 3000,
     app = express();
 
 //Serving the files on the dist folder
